66edbe469c39cddafa8c263eb2defda6cc91c98e
Abdulbari Qaisar <abdulbari.112917@gmail.com>
2022-10-14 21:51:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1")
+
+newHeader.innerHTML= "Abdulbari is the champion"
+newHeader.setAttribute("id" , "victory")
+document.append(newHeader)
